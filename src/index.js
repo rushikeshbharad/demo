9bac0308110f194f2dd506941e038ca23c94a11c
Rushikesh Bharad <rushicbharad@gmail.com>
2024-12-16 17:32:10 +0530

    Added gh-token for gh-pages branch to package.json

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from "react-router";
+import { BrowserRouter, Routes, Route } from 'react-router';
 import './index.css';
 import Home from './pages/home';
 import reportWebVitals from './reportWebVitals';
